11f7088f75fb01c3576531f1e4a1fdc9bf157c95
milrosen <61394098+milrosen@users.noreply.github.com>
2020-07-05 09:38:11 -0700

    added explosions

--- bootstrap.js
+++ bootstrap.js
@@ -237,7 +237,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rust_js_asteroids_bg.wasm":"cdce6d20e488d54aa43e"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/rust_js_asteroids_bg.wasm":"d7509a9a9ba08d582cd8"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
